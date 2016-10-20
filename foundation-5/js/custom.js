97e96e998d42156e4d2b94ff0e7ef984459ec7eb
Greg Schultz <gjs86@cornell.edu>
2016-10-20 12:49:15 -0400

    some logo changes and jquery mods

--- foundation-5/js/custom.js
+++ foundation-5/js/custom.js
@@ -1,10 +1,26 @@
 // custom.js
 
+// middle calculator
+// var currWidth = $(window).width();
+// console.log(currWidth);
+// var startPos = -100;
+// var endPos = (currWidth / 2) + (startPos / 2);
+// console.log(endPos);
+
+
 $(function(){
     $('.card').animate({'margin-top': '100px'}, 1000);
     $('.card').fadeIn("slow", 400);
     // turn off display nones
     $('.profile').show(400);
     $('.txt_hide').show(400);
-    $('.profile').css('margin-top','0');
+    // $('.profile').css('margin-top','0');
+
+
+    // $('.left_fly').animate({'margin-left':-100 }, 400);
+	// $('.right_fly').animate({'margin-right': }, 400);
 });
+
+
+
+
