a1f5db430afbd735c05461784916f8c81f8a4e66
Greg Schultz <gjs86@cornell.edu>
2016-10-20 00:18:18 -0400

    rough skeleton of home and teams page with header and footer

---
+++ foundation-5/js/custom.js
@@ -0,0 +1,10 @@
+// custom.js
+
+$(function(){
+    $('.card').animate({'margin-top': '100px'}, 1000);
+    $('.card').fadeIn("slow", 400);
+    // turn off display nones
+    $('.profile').show(400);
+    $('.txt_hide').show(400);
+    $('.profile').css('margin-top','0');
+});
